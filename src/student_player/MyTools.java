0bad91d9f1c2ddfa45fd48bc24f2bee976cf4ef6
LeaCollin711 <leac750@gmail.com>
2018-04-05 17:49:43 -0400

    had to reset project, have minimax implemented

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,235 @@
 package student_player;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import boardgame.Move;
+import coordinates.Coord;
+import coordinates.Coordinates;
+import tablut.TablutBoardState;
+import tablut.TablutMove;
+
 public class MyTools {
-    public static double getSomething() {
-        return Math.random();
+    
+public static int evaluateBoard(TablutBoardState bs, TablutBoardState clonedBS) {
+    	
+    	int score = 0;
+    	int captured = 0;
+    	int captureCenter = 0;
+    	int player_id = clonedBS.getTurnPlayer();
+    	    	
+    	 // look at opponent and its number of pieces
+        int opponent = clonedBS.getOpponent();
+        int minNumberOfOpponentPieces = bs.getNumberPlayerPieces(opponent);
+
+        // will be used for checking if capture is possible
+        Coord center = Coordinates.get(4,4);
+        Coord startKingPos = bs.getKingPosition();
+        Coord endKingPos = clonedBS.getKingPosition();
+        
+        List<Coord> corners = Coordinates.getCorners();
+        List<Coord> centerNeighbors = Coordinates.getNeighbors(center);
+        List<Coord> cornerNeighbors = new ArrayList<>();
+        
+        for (Coord corner : corners) {
+        	for (Coord neighbor : Coordinates.getNeighbors(corner)) {
+        		cornerNeighbors.add(neighbor);
+        	}
+        }
+    	
+        // Check how many opponent pieces there are now, maybe we captured some!
+        int newNumberOfOpponentPieces = clonedBS.getNumberPlayerPieces(opponent);
+
+        int numberOfPieces = clonedBS.getNumberPlayerPieces(player_id);
+        
+        if (newNumberOfOpponentPieces < minNumberOfOpponentPieces) {
+        	int numberCaptured = minNumberOfOpponentPieces - newNumberOfOpponentPieces; 
+        	// could change amount I multiply this by 
+        	if(player_id == TablutBoardState.MUSCOVITE) {
+        		captured = numberCaptured*5; 
+        	}
+        	else {
+        		captured = numberCaptured*3;
+        	}
+        }
+        
+        // for all positions next to the center, see if you can get rid of an enemy
+        //  (prioritize this over other kinds of captures)
+        
+        for (Coord neighbor : centerNeighbors) {
+        	if (bs.isOpponentPieceAt(neighbor) && !clonedBS.isOpponentPieceAt(neighbor)) {
+        		captureCenter = 20;
+        	}
+        }
+        
+        if(player_id == TablutBoardState.SWEDE) {
+        	for (Coord neighbor : cornerNeighbors) {
+            	if (bs.isOpponentPieceAt(neighbor) && clonedBS.coordIsEmpty(neighbor)) {
+            		score += 100;
+            	}
+            }
+        }
+    
+        
+        int minDistance = Coordinates.distanceToClosestCorner(startKingPos);
+
+        score += captured;
+        score -= newNumberOfOpponentPieces*100;
+        score += numberOfPieces*150;
+        
+        if(player_id == TablutBoardState.MUSCOVITE) {
+            score += captureCenter; 
+            score -= minDistance;
+        } 
+        
+        else {
+        	score += minDistance;
+        }
+        
+        try {
+	        List<Coord> kingNeighbors = Coordinates.getNeighbors(endKingPos);
+		    int moveDistance = Coordinates.distanceToClosestCorner(endKingPos);
+		    int kingScore = 0;
+		    List<Boolean> presentOpponents = new ArrayList<>();
+		    for (Coord neighbor : kingNeighbors) {
+		    	if (clonedBS.isOpponentPieceAt(neighbor)) {
+		    		presentOpponents.add(true);
+		    	}
+		    }
+		    
+		   if (moveDistance < minDistance && !cornerNeighbors.contains(endKingPos) && presentOpponents.isEmpty()) {
+		        minDistance = moveDistance;
+		        kingScore += 20;
+		    }
+		    else {
+		    	kingScore -= 30;
+		    }
+		    
+		    HashSet<Coord> opponentsAt = clonedBS.getOpponentPieceCoordinates();
+		    
+		    // check to see if final position is at the edge (but is not a corner
+		   if ((endKingPos.x == 0 || endKingPos.x == 8 || endKingPos.y == 0 || endKingPos.y == 8) && !cornerNeighbors.contains(endKingPos)) {
+		    	for(Coord enemy : opponentsAt) {
+	    			if (enemy.x != 0 || enemy.x != 8 || enemy.y != 8 || enemy.y != 8) {
+	        			kingScore += 3000;
+	        		}
+		    	}
+		    
+		   }
+		    
+		    if(cornerNeighbors.contains(endKingPos)) {
+		    	kingScore = -30000;
+		    }
+		    if(Coordinates.isCorner(endKingPos)) {
+		    	kingScore = 30000;
+		    }
+		    
+		    
+		    if(player_id == TablutBoardState.SWEDE) {
+		    	score += kingScore;
+		    }
+		    
+		    else {
+		    	score -= kingScore;
+		    }
+		    
+		    }
+        catch(Exception e) {
+        	
+        }
+
+        // if the moves leads to winning ... do it
+        if (clonedBS.getWinner() == player_id) {
+            score = Integer.MAX_VALUE;
+        }
+    	
+		return score;
+    	
     }
+    
+    public static Move minimax(TablutBoardState state, int player) {
+    	Move myMove = state.getRandomMove();
+    	int depth = 0;
+    	int bestScore = Integer.MIN_VALUE;
+    	
+    	List<TablutMove> options = state.getAllLegalMoves();
+    	
+    	for(TablutMove move : options) {
+    		TablutBoardState cloneBS = (TablutBoardState) state.clone();
+
+            // Process that move, as if we actually made it happen.
+            cloneBS.processMove(move);
+           // System.out.println("Original state turn player : " + state.getTurnPlayer());
+            // System.out.println("Cloned state turn player: " + cloneBS.getTurnPlayer());
+            int moveScore = MIN(state, cloneBS, depth + 1, player);
+            if (moveScore > bestScore) {
+            	bestScore = moveScore;
+            	myMove = move;
+            }
+    	}
+    	
+    	return myMove;
+    }
+    
+    public static int MIN(TablutBoardState state, TablutBoardState clonedBS, int depth, int player) {
+    	int bestScore;
+    	if(clonedBS.getWinner() == 1 || clonedBS.getWinner() == 0) {
+    		if(clonedBS.getWinner() == player) {
+    			return Integer.MAX_VALUE;
+    		}
+    		else {
+    			return Integer.MIN_VALUE;
+    		}
+    	}
+    	else if (depth == 2) {
+    		return -1*evaluateBoard(state, clonedBS);
+    	}
+    	else {
+    		bestScore = Integer.MAX_VALUE;
+    		List<TablutMove> options = clonedBS.getAllLegalMoves();
+    		for(TablutMove move : options) {
+    			TablutBoardState bs = (TablutBoardState) clonedBS.clone();
+    			bs.processMove(move);
+    			int moveScore = MAX(clonedBS, bs, depth + 1, player);
+    			if(moveScore < bestScore) {
+    				bestScore = moveScore;
+    			}
+    		}
+    		
+    	}
+    	
+    	return bestScore;
+    }
+    
+    public static int MAX(TablutBoardState state, TablutBoardState clonedBS, int depth, int player) {
+    	int bestScore;
+    	if(clonedBS.getWinner() == 1 || clonedBS.getWinner() == 0) {
+    		if(clonedBS.getWinner() == player) {
+    			return Integer.MAX_VALUE;
+    		}
+    		else {
+    			return Integer.MIN_VALUE;
+    		}
+    	}
+    	else if (depth == 2) {
+    		return evaluateBoard(state, clonedBS);
+    	}
+    	else {
+    		bestScore = Integer.MIN_VALUE;
+    		List<TablutMove> options = clonedBS.getAllLegalMoves();
+    		for(TablutMove move : options) {
+    			TablutBoardState bs = (TablutBoardState) clonedBS.clone();
+    			bs.processMove(move);
+    			int moveScore = MIN(clonedBS, bs, depth + 1, player);
+    			if(moveScore > bestScore) {
+    				bestScore = moveScore;
+    			}
+    		}
+    		
+    	}
+    	
+    	return bestScore;
+    }
+
 }
--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -1,19 +1,22 @@
 package student_player;
 
 import boardgame.Move;
+import boardgame.Player;
 import tablut.TablutBoardState;
+import tablut.TablutMove;
 import tablut.TablutPlayer;
+import tablut.RandomTablutPlayer;
+import tablut.GreedyTablutPlayer;
 
 /** A player file submitted by a student. */
 public class StudentPlayer extends TablutPlayer {
-
     /**
      * You must modify this constructor to return your student number. This is
      * important, because this is what the code that runs the competition uses to
      * associate you with your agent. The constructor should do nothing else.
      */
     public StudentPlayer() {
-        super("xxxxxxxxx");
+        super("260618407");
     }
 
     /**
@@ -21,16 +24,54 @@ public class StudentPlayer extends TablutPlayer {
      * object contains the current state of the game, which your agent must use to
      * make decisions.
      */
-    public Move chooseMove(TablutBoardState boardState) {
-        // You probably will make separate functions in MyTools.
-        // For example, maybe you'll need to load some pre-processed best opening
-        // strategies...
-        MyTools.getSomething();
-
+    public Move chooseMove(TablutBoardState bs) {
         // Is random the best you can do?
-        Move myMove = boardState.getRandomMove();
+        Move myMove = bs.getRandomMove();
 
+        myMove = MyTools.minimax(bs, bs.getTurnPlayer());
+        
+        // however if the Muscovites are going first, always do same first move
+        if (player_id == TablutBoardState.MUSCOVITE) {
+        	
+        	if (bs.getTurnNumber() == 0) {
+        		
+        		// always start with this move
+        		myMove = new TablutMove(7, 4, 7, 3, player_id);
+        	}
+        }
+        
         // Return your move to be processed by the server.
         return myMove;
+	}
+        
+
+	// For Debugging purposes only.
+    public static void main(String[] args) {
+    	int i = 0;
+    	int numWins = 0;
+    	while(i < 500) {
+    		TablutBoardState b = new TablutBoardState();
+            Player swede = new GreedyTablutPlayer();
+            swede.setColor(TablutBoardState.SWEDE);
+
+            Player muscovite = new StudentPlayer();
+            muscovite.setColor(TablutBoardState.MUSCOVITE);
+            
+            Player player = muscovite;
+            while (!b.gameOver()) {
+                Move m = player.chooseMove(b);
+                b.processMove((TablutMove) m);
+                player = (player == muscovite) ? swede : muscovite;
+                //System.out.println("\nMOVE PLAYED: " + m.toPrettyString());
+                //b.printBoard();
+            }
+            if(b.getWinner() == 1) {
+            	numWins++;
+            }
+            //System.out.println(TablutMove.getPlayerName(b.getWinner()) + " WIN!");
+            i++;
+    	}
+    	System.out.println("Number of wins: " + numWins);
+        
     }
 }
